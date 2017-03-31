f7ef86d09432730ccda0c748c1dbd8feb167db71
Niklas Ek <nikek@spotify.com>
2017-03-31 15:13:22 +0200

    [storybook] add storybook and storyshots

--- src/SparkBar.jsx
+++ src/SparkBar.jsx
@@ -4,34 +4,34 @@ import { scaleLinear } from 'd3-scale';
 const defaultWidth = 160;
 const defaultHeight = 90;
 const defaultColor = '#2ebd59';
+const gapRatio = 0.15;
 
 export default class SparkBar extends React.Component {
   render() {
-    const data = this.props.data;
-    const chartW = this.props.width || defaultWidth;
-    const chartH = this.props.height || defaultHeight;
-    const barColor = this.props.color || defaultColor;
-
+    const data = this.props.data || [];
+    const width = this.props.width || defaultWidth;
+    const height = this.props.height || defaultHeight;
+    const color = this.props.color || defaultColor;
+    const gapCount = data.length - 1;
+    const gapW = width / gapCount * gapRatio;
+    const barW = (width - gapW * gapCount) / data.length;
+    const offset = barW + gapW;
     const yScale = scaleLinear()
       .domain([0, Math.max(...data.map(p => p.y))])
-      .range([0, chartH]);
-
-    const barW = chartW / data.length * 0.9;
-    const gapW = chartW / data.length * 0.1;
-    const offset = barW + gapW;
+      .range([0, height]);
 
     return (
-      <svg width={chartW} height={chartH}>
+      <svg width={width} height={height}>
         {data.map((d, i) => {
-          const barHeight = yScale(d.y);
+          const barH = yScale(d.y);
           return (
             <rect
-              key={d.x}
+              key={i}
               x={i * offset}
-              y={chartH - barHeight}
-              height={barHeight}
+              y={height - barH}
+              height={barH}
               width={barW}
-              fill={barColor}
+              fill={color}
             />
           );
         })}
